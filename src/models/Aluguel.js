07f4e8c7da33f7425b5884b6588d71e0090033f7
Drkphl <leonardotizon@hotmail.com>
2024-09-04 12:41:42 -0300

    consertado data fim e validação

--- src/models/Aluguel.js
+++ src/models/Aluguel.js
@@ -5,29 +5,14 @@ let nextId = 1;
 
 const validarLivros = (Livros) => Livros.every((Livro) => Livro_controller.show(Livro.Livro_id));
 
-const duracao = (Livros) => {
-    let soma = 0;
-
-    Livros.forEach((Livro) => {
-        soma += 7;
-    });
-
-    return soma;
-};
-
 const model = (body, id = nextId++) => {
-    if (
-        body.data_inicio != undefined &&
-        body.data_inicio >= 0 &&
-        Array.isArray(body.Livros) &&
-        validarLivros(body.Livros)
-    ) {
+    if (body.data_inicio != undefined && Estudante_controller.show(body.Estudante_id) && Array.isArray(body.Livros) && validarLivros(body.Livros)) {
         return {
             id,
             data_inicio: body.data_inicio,
             Estudante_id: body.Estudante_id,
             Livros: body.Livros,
-            data_fim: body.data_inicio + duracao(body.Livros),
+            data_fim: body.data_fim,
         };
     }
 };
